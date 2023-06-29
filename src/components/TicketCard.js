238d10155bf1084369fe5a44a1d4c388bd989244
Lucas Toh <tohhylucas@gmail.com>
2023-06-29 11:40:55 +0800

    Supabase Integration with Portals

---
+++ src/components/TicketCard.js
@@ -0,0 +1,16 @@
+const TicketCard = ({ ticket }) => {
+  return (
+    <div className="ticket-card">
+      <p>{ticket.Name}</p>
+      <p>{ticket.LandlordID}</p>
+      <p>{ticket.TenantID}</p>
+      <p>{ticket.Contact}</p>
+      <p>{ticket.SubmittedDateTime}</p>
+      <p>{ticket.RequestType}</p>
+      <p>{ticket.RequestDescription}</p>
+      <p>{ticket.Status}</p>
+    </div>
+  );
+};
+
+export default TicketCard;
