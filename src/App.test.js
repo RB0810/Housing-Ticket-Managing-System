7472083716284adb5aadbe4778070ac38bf88975
NoraLeo <dheenaleo2512@gmail.com>
2023-07-03 16:39:39 +0800

    Add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+//test components/NavBar.js
+test('renders NavBar', () => {
+  render(<NavBar />);
+  const linkElement = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
-});
+  });
+
+//test components/TicketCard.js
+test('renders TicketCard', () => {
+  render(<TicketCard />);
+  const linkElement = screen.getByText(/ticket/i);
+  expect(linkElement).toBeInTheDocument();
+  });
+
+//test components in components/login.js
+test('renders login', () => {
+  render(<Login />);
+  const linkElement = screen.getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+  });
+
+//test individual components in pages/portal/landlordportal.js
+test('renders landlordportal', () => {
+  render(<LandlordPortal />);
+  const linkElement = screen.getByText(/landlordportal/i);
+  expect(linkElement).toBeInTheDocument();
+  });
+
+//test individual components in pages/portal/tenantportal.js
+test('renders tenantportal', () => {
+
+  render(<TenantPortal />);
+  const linkElement = screen.getByText(/tenantportal/i);
+  expect(linkElement).toBeInTheDocument();
+  });
