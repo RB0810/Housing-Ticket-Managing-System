8d2a0f55511911ef6f985e3ad48af53add25adc6
NoraLeo <38717394+NoraLeo@users.noreply.github.com>
2023-07-11 18:12:02 +0800

    Update Notification.js

--- src/Notification.js
+++ src/Notification.js
@@ -1,6 +1,10 @@
-
+const API_KEY = '7726fdc285aaa0f802800905c0f6a8f5-6d8d428c-6e658693';
+const DOMAIN = 'sandbox0030cea2ea9e4001a5a0c71b0a64ab88.mailgun.org';
 import formData from 'form-data';
+import Mailgun from 'mailgun.js';
 
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({ username: 'api', key: API_KEY });
 class Notification {
   constructor() {}
 
