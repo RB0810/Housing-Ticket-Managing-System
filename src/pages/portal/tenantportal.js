c6ed958094e99b48d99b0313786e2d9ff8630678
Rishika Banka <91309421+RB0810@users.noreply.github.com>
2023-07-15 16:52:30 +0530

    tenant filtering done

--- src/pages/portal/tenantportal.js
+++ src/pages/portal/tenantportal.js
@@ -1,40 +1,131 @@
-import { useEffect, useState } from "react";
-import { Routes, Route, useParams, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import TicketManager from "../../managers/ticketmanager";
 import "../../styles/ticketportal.css";
 
-export default function TenantPortal() {
+const TenantPortal = () => {
   const ticketManager = new TicketManager();
-  let { PARCStatus, TenantID } = useParams();
+  const { PARCStatus, TenantID } = useParams();
   const [serviceTickets, setServiceTickets] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [fetchError, setFetchError] = useState(null);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
+  const [requestFilter, setRequestFilter] = useState("");
+  const [assignedToFilter, setAssignedToFilter] = useState("");
+  const [dateFilter, setDateFilter] = useState("");
+  const [dateSortOrder, setDateSortOrder] = useState("");
+  const [isCategoryFilterOpen, setIsCategoryFilterOpen] = useState(false);
+  const [isStatusFilterOpen, setIsStatusFilterOpen] = useState(false);
+  const [isRequestFilterOpen, setIsRequestFilterOpen] = useState(false);
+  const [isAssignedToFilterOpen, setIsAssignedToFilterOpen] = useState(false);
+  const [isDateFilterOpen, setIsDateFilterOpen] = useState(false);
 
   useEffect(() => {
-    const getTickets = async () => {
-      try {
-        let data = await ticketManager.getTicketsByPARCStatusForTenantID(
-          PARCStatus.toUpperCase(),
-          parseInt(TenantID)
-        );
-
-        console.log(data);
-        setServiceTickets(data);
-        setFetchError(null);
-      } catch (error) {
-        console.error("Error fetching tickets:", error);
-        setFetchError("Error!");
-        setServiceTickets([]);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     getTickets();
   }, [PARCStatus, TenantID]);
 
+  const getTickets = async () => {
+    try {
+      let data = await ticketManager.getTicketsByPARCStatusForTenantID(
+        PARCStatus.toUpperCase(),
+        parseInt(TenantID)
+      );
+
+      console.log(data);
+      setServiceTickets(data);
+      setFetchError(null);
+    } catch (error) {
+      console.error("Error fetching tickets:", error);
+      setFetchError("Error!");
+      setServiceTickets([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const getViewTicketsRoute = () => {
     return "/tenantportal/ticket";
+  };
+
+  const isDateInRange = (submittedDate) => {
+    if (dateFilter === "") {
+      return true;
+    }
+
+    const currentDate = new Date(submittedDate);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    if (dateFilter === "newest") {
+      return currentDate >= today;
+    } else if (dateFilter === "oldest") {
+      return currentDate < today;
+    }
+
+    return true;
+  };
+
+  const filteredTickets = serviceTickets.filter((ticket) => {
+    if (categoryFilter && ticket.Category !== categoryFilter) {
+      return false;
+    }
+    if (statusFilter && ticket.Status !== statusFilter) {
+      return false;
+    }
+    if (requestFilter && !ticket.Name.toLowerCase().includes(requestFilter.toLowerCase())) {
+      return false;
+    }
+    if (assignedToFilter && ticket.staffDetails && !ticket.staffDetails.StaffName.toLowerCase().includes(assignedToFilter.toLowerCase())) {
+      return false;
+    }
+    return true;
+  }).sort((a, b) => {
+    if (dateFilter === "newest") {
+      return new Date(b.SubmittedDateTime) - new Date(a.SubmittedDateTime);
+    } else if (dateFilter === "oldest") {
+      return new Date(a.SubmittedDateTime) - new Date(b.SubmittedDateTime);
+    }
+    return 0;
+  });
+  
+
+  const removeFilters = () => {
+    setCategoryFilter("");
+    setIsCategoryFilterOpen(false);
+    setStatusFilter("");
+    setIsStatusFilterOpen(false);
+    setRequestFilter("");
+    setIsRequestFilterOpen(false);
+    setAssignedToFilter("");
+    setIsAssignedToFilterOpen(false);
+    setDateFilter("");
+    setIsDateFilterOpen(false);
+    setDateSortOrder("");
+    setIsLoading(true);
+    setFetchError(null);
+
+    getTickets();
+  };
+
+  const sortTicketsByDate = (order) => {
+    setDateSortOrder(order);
+
+    const sortedTickets = [...filteredTickets];
+    sortedTickets.sort((a, b) => {
+      const dateA = new Date(a.SubmittedDateTime);
+      const dateB = new Date(b.SubmittedDateTime);
+
+      if (order === "newest") {
+        return dateB - dateA;
+      } else if (order === "oldest") {
+        return dateA - dateB;
+      }
+
+      return 0;
+    });
+
+    setServiceTickets(sortedTickets);
   };
 
   return (
@@ -43,30 +134,143 @@ export default function TenantPortal() {
         <thead>
           <tr className="table-row">
             <th className="header-cell text-center">S No.</th>
-            <th className="header-cell text-center">Request</th>
-            <th className="header-cell text-center">Category</th>
+            <th className="header-cell text-center">
+              <div className="filter-header">
+                <span>Request</span>
+                <button
+                  className={`filter-arrow ${isRequestFilterOpen ? "up" : ""}`}
+                  onClick={() => setIsRequestFilterOpen(!isRequestFilterOpen)}
+                ></button>
+                {isRequestFilterOpen && (
+                  <div className="filter-dropdown">
+                    <input
+                      type="text"
+                      value={requestFilter}
+                      onChange={(e) => setRequestFilter(e.target.value)}
+                      placeholder="Filter by request..."
+                    />
+                  </div>
+                )}
+              </div>
+            </th>
+            <th className="header-cell text-center">
+              <div className="filter-header">
+                <span>Category</span>
+                <button
+                  className={`filter-arrow ${isCategoryFilterOpen ? "up" : ""}`}
+                  onClick={() => setIsCategoryFilterOpen(!isCategoryFilterOpen)}
+                ></button>
+                {isCategoryFilterOpen && (
+                  <div className="filter-dropdown">
+                    <select
+                      value={categoryFilter}
+                      onChange={(e) => setCategoryFilter(e.target.value)}
+                    >
+                      <option value="">All</option>
+                      <option value="Plumbing">Plumbing</option>
+                      <option value="Toilet">Toilet</option>
+                      <option value="Pest">Pest</option>
+                      <option value="Electric">Electric</option>
+                      <option value="Aircon">Aircon</option>
+                      <option value="Others">Others</option>
+                    </select>
+                  </div>
+                )}
+              </div>
+            </th>
             <th className="header-cell text-center">Unit</th>
-            <th className="header-cell text-center">Status</th>
-            <th className="header-cell text-center">Submitted Date</th>
-            <th className="header-cell text-center">Assigned To</th>
+            <th className="header-cell text-center">
+              <div className="filter-header">
+                <span>Status</span>
+                <button
+                  className={`filter-arrow ${isStatusFilterOpen ? "up" : ""}`}
+                  onClick={() => setIsStatusFilterOpen(!isStatusFilterOpen)}
+                ></button>
+                {isStatusFilterOpen && (
+                  <div className="filter-dropdown">
+                    <select
+                      value={statusFilter}
+                      onChange={(e) => setStatusFilter(e.target.value)}
+                    >
+                      <option value="">All</option>
+                      <option value="Awaiting Review">Awaiting Review</option>
+                      <option value="Ticket Assigned">Ticket Assigned</option>
+                      <option value="Quotation Uploaded">Quotation Uploaded</option>
+                      <option value="Quotation Accepted">Quotation Accepted</option>
+                      <option value="Quotation Rejected">Quotation Rejected</option>
+                      <option value="Works Started">Works Started</option>
+                      <option value="Works Ended">Works Ended</option>
+                      <option value="Works Rejected">Works Rejected</option>
+                      <option value="Feedback Submitted">Feedback Submitted</option>
+                    </select>
+                  </div>
+                )}
+              </div>
+            </th>
+            <th className="header-cell text-center">
+              <div className="filter-header">
+                <span>Submitted Date</span>
+                <button
+                  className={`filter-arrow ${isDateFilterOpen ? "up" : ""}`}
+                  onClick={() => setIsDateFilterOpen(!isDateFilterOpen)}
+                ></button>
+                {isDateFilterOpen && (
+                  <div className="filter-dropdown">
+                    <select
+                      value={dateFilter}
+                      onChange={(e) => setDateFilter(e.target.value)}
+                    >
+                      <option value="">Sort according to Date Submitted</option>
+                      <option value="newest">Newest to Oldest</option>
+                      <option value="oldest">Oldest to Newest</option>
+                    </select>
+                  </div>
+                )}
+              </div>
+            </th>
+            <th className="header-cell text-center">
+              <div className="filter-header">
+                <span>Assigned To</span>
+                <button
+                  className={`filter-arrow ${isAssignedToFilterOpen ? "up" : ""}`}
+                  onClick={() => setIsAssignedToFilterOpen(!isAssignedToFilterOpen)}
+                ></button>
+                {isAssignedToFilterOpen && (
+                  <div className="filter-dropdown">
+                    <input
+                      type="text"
+                      value={assignedToFilter}
+                      onChange={(e) => setAssignedToFilter(e.target.value)}
+                      placeholder="Filter by assigned..."
+                    />
+                  </div>
+                )}
+              </div>
+            </th>
             <th className="header-cell text-center"></th>
           </tr>
         </thead>
         <tbody>
-        {isLoading ? (
+          {isLoading ? (
             <tr>
-              <td colSpan="8" className="text-center">Loading...</td>
+              <td colSpan="8" className="text-center">
+                Loading...
+              </td>
             </tr>
           ) : fetchError ? (
             <tr>
-              <td colSpan="8" className="text-center">{fetchError}</td>
+              <td colSpan="8" className="text-center">
+                {fetchError}
+              </td>
             </tr>
-          ) : serviceTickets.length === 0 ? (
+          ) : filteredTickets.length === 0 ? (
             <tr>
-              <td colSpan="8" className="text-center">Empty!</td>
+              <td colSpan="8" className="text-center">
+                Empty!
+              </td>
             </tr>
           ) : (
-            serviceTickets.map((ticket, index) => (
+            filteredTickets.map((ticket, index) => (
               <tr key={ticket.ServiceRequestID} className="table-row">
                 <td className="text-center">{index + 1}</td>
                 <td className="text-center">{ticket.Name}</td>
@@ -87,6 +291,11 @@ export default function TenantPortal() {
           )}
         </tbody>
       </table>
+      <button onClick={removeFilters}>
+        Remove all Filters
+      </button>
     </div>
   );
-}
+};
+
+export default TenantPortal;
