2d1f28140d9ba77cb31a2842edc301fefc6583f9
Lucas Toh <tohhylucas@gmail.com>
2023-07-15 14:59:13 +0800

    Update Supervisor Portal

--- src/components/SupervisorTicketDetails.js
+++ src/components/SupervisorTicketDetails.js
@@ -3,6 +3,8 @@ import SubmittedByCard from "./SubmittedByCard";
 import AssignedToCard from "./AssignedToCard";
 import TicketAssigner from "./TicketAssigner";
 import BasicTicketDetails from "./BasicTicketDetails";
+import ViewRejectDetails from "./ViewRejectDetails";
+import ViewFinalFeedbackDetails from "./ViewFinalFeedbackDetails";
 
 const SupervisorTicketDetails = ({ ticket, tenant, staff, portal, status }) => {
   // Ticket Details is Changed Based on Portal and Status
@@ -27,6 +29,71 @@ const SupervisorTicketDetails = ({ ticket, tenant, staff, portal, status }) => {
         <SubmittedByCard tenant={tenant} />
         ____________________________________
         <AssignedToCard staff={staff} />
+      </div>
+    );
+  }
+
+  if (portal === "supervisor" && status === "Works Started") {
+    return (
+      <div>
+        <BasicTicketDetails ticket={ticket} />
+        _______________________________________
+        <SubmittedByCard tenant={tenant} />
+        ____________________________________
+        <AssignedToCard staff={staff} />
+      </div>
+    );
+  }
+
+  if (portal === "supervisor" && status === "Works Ended") {
+    return (
+      <div>
+        <BasicTicketDetails ticket={ticket} />
+        _______________________________________
+        <SubmittedByCard tenant={tenant} />
+        ____________________________________
+        <AssignedToCard staff={staff} />
+      </div>
+    );
+  }
+
+  if (portal === "supervisor" && status === "Works Rejected") {
+    return (
+      <div>
+        <BasicTicketDetails ticket={ticket} />
+        _______________________________________
+        <SubmittedByCard tenant={tenant} />
+        ____________________________________
+        <AssignedToCard staff={staff} />
+        _______________________________________
+      </div>
+    );
+  }
+
+  if (portal === "supervisor" && status === "Works Rejected") {
+    return (
+      <div>
+        <BasicTicketDetails ticket={ticket} />
+        _______________________________________
+        <SubmittedByCard tenant={tenant} />
+        ____________________________________
+        <AssignedToCard staff={staff} />
+        _______________________________________
+        <ViewRejectDetails ticket={ticket} />
+      </div>
+    );
+  }
+
+  if (portal === "supervisor" && status === "Feedback Submitted") {
+    return (
+      <div>
+        <BasicTicketDetails ticket={ticket} />
+        _______________________________________
+        <SubmittedByCard tenant={tenant} />
+        ____________________________________
+        <AssignedToCard staff={staff} />
+        _______________________________________
+        <ViewFinalFeedbackDetails ticket={ticket} />
       </div>
     );
   }
