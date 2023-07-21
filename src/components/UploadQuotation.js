f41abcd244e32e26672801b84003aca39cfa9114
Lucas Toh <tohhylucas@gmail.com>
2023-07-21 18:15:33 +0800

    Update file upload

--- src/components/UploadQuotation.js
+++ src/components/UploadQuotation.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import supabase from "../config/supabaseClient";
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2'; 
 
 const UploadQuotation = ({ bucketName, ServiceRequestID }) => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -11,64 +11,60 @@ const UploadQuotation = ({ bucketName, ServiceRequestID }) => {
 
   const uploadFileToBucket = async () => {
     if (!selectedFile) {
-      console.log("No file selected.");
+      console.log('No file selected.');
       return;
     }
 
     const fileName = selectedFile.name;
-    const fileExt = fileName.split(".").pop();
+    const fileExt = fileName.split('.').pop();
     const filePath = `${ServiceRequestID}.${fileExt}`;
 
     try {
       const { data, error } = await supabase.storage
         .from(bucketName)
-        .upload(filePath, selectedFile, { cacheControl: "3600", upsert: true });
+        .upload(filePath, selectedFile, { cacheControl: '3600', upsert: false });
 
       if (error) {
-        console.error("Error uploading file:", error);
-        Swal.fire({
-          // Display error alert
-          icon: "error",
-          title: "Oops...",
-          text: "Something went wrong!",
+        console.error('Error uploading file:', error);
+        Swal.fire({  // Display error alert
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!'
         });
         return { error };
       } else {
         console.log(`File path is: ${filePath}`);
-        console.log("File uploaded successfully:", data);
+        console.log('File uploaded successfully:', data);
 
         const { data: updateData, error: updateError } = await supabase
-          .from("Service Request")
-          .update({ QuotationAttachmentPath: filePath })
-          .match({ ServiceRequestID: ServiceRequestID });
+        .from('Service Request')
+        .update({ QuotationAttachmentPath: filePath })
+        .match({ ServiceRequestID: ServiceRequestID });
 
         if (updateError) {
-          console.error("Error updating file path:", updateError);
-          Swal.fire({
-            // Display error alert
-            icon: "error",
-            title: "Oops...",
-            text: "Failed to update the file path in the database!",
+          console.error('Error updating file path:', updateError);
+          Swal.fire({  // Display error alert
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Failed to update the file path in the database!'
           });
           return { error: updateError };
         } else {
-          console.log("File path updated successfully:", updateData);
-          Swal.fire({
-            // Display success alert
-            icon: "success",
-            title: "Uploaded",
-            text: "Your file has been uploaded and the file path is updated.",
+          console.log('File path updated successfully:', updateData);
+          Swal.fire({  // Display success alert
+            icon: 'success',
+            title: 'Uploaded',
+            text: 'Your file has been uploaded and the file path is updated.'
           });
           return { data: updateData };
         }
       }
     } catch (error) {
-      console.error("Error uploading file:", error);
-      Swal.fire({
-        // Display error alert
-        icon: "error",
-        title: "Oops...",
-        text: "Something went wrong!",
+      console.error('Error uploading file:', error);
+      Swal.fire({  // Display error alert
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!'
       });
       return { error };
     }
--- src/pages/staff/viewticket.js
+++ src/pages/staff/viewticket.js
@@ -7,6 +7,12 @@ import ViewFinalFeedbackDetails from "../../components/ViewFinalFeedbackDetails"
 import { useParams } from "react-router-dom";
 import UploadQuotation from "../../components/UploadQuotation";
 import DisplayQuotation from "../../components/DisplayQuotation";
+import supabase from "../../config/supabaseClient";
+import Swal from "sweetalert2";
+
+// Quotation-related imports
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const ViewTicketStaff = () => {
   const ticketManager = new TicketManager();
@@ -20,6 +26,10 @@ const ViewTicketStaff = () => {
   const [feedbackComments, setFeedbackComments] = useState("");
 
   const [quotationRequired, setQuotationRequired] = useState("true");
+
+  // Quotation Items
+  const [quotationPath, setQuotationPath] = useState(null);
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     const getTenantAndTicket = async () => {
@@ -37,9 +47,25 @@ const ViewTicketStaff = () => {
         setStatus(ticketData[0].Status);
         setFeedbackComments(ticketData[0].FeedbackComments);
         setQuotationRequired(ticketData[0].QuotationRequired);
+        setQuotationPath(ticketData[0].QuotationAttachmentPath);
+        if (quotationPath !== null) {
+          if (quotationPath.endsWith(".pdf")) {
+            const { data: fileData, error: fileError } = await supabase.storage
+              .from("quotation")
+              .download(quotationPath);
+
+            if (fileError || !fileData) {
+              console.error("Error downloading file:", fileError);
+            } else {
+              const url = URL.createObjectURL(fileData);
+              setFile(url);
+            }
+          }
+        }
         setFetchError(null);
       } else if (ticketData.length === 0) {
         setFetchError("This ticket is EMPTY!");
+        window.alert("Ticket empty!");
         setServiceTicket();
       } else {
         setFetchError("Error finding this ticket :(");
@@ -57,8 +83,6 @@ const ViewTicketStaff = () => {
 
   const handleContinue = async () => {
     try {
-      if (quotationRequired === true) {
-      }
       // Get promises for each update
       const updateQuotationRequiredPromise = ticketManager.updateTicket(
         serviceTicket.ServiceRequestID,
@@ -149,15 +173,107 @@ const ViewTicketStaff = () => {
     }
   };
 
+  const handleFileChange = async (event) => {
+    await uploadFileToBucket(event.target.files[0]);
+  };
+
+  const uploadFileToBucket = async (file) => {
+    if (!file) {
+      console.log("No file selected.");
+      return;
+    }
+
+    const fileName = file.name;
+    const fileExt = fileName.split(".").pop();
+    const filePath = `${serviceTicket.ServiceRequestID}.${fileExt}`;
+
+    try {
+      // Wait for the file upload to complete
+      const { data, error } = await supabase.storage
+        .from("quotation")
+        .upload(filePath, file, { cacheControl: "3600", upsert: true });
+
+      if (error) {
+        console.error("Error uploading file:", error);
+        Swal.fire({
+          // Display error alert
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+        return { error };
+      }
+
+      console.log(`File path is: ${filePath}`);
+      console.log("File uploaded successfully:", data);
+
+      // Wait for the database update to complete
+      const { data: updateData, error: updateError } = await supabase
+        .from("Service Request")
+        .update({ QuotationAttachmentPath: filePath })
+        .match({ ServiceRequestID: serviceTicket.ServiceRequestID });
+
+      if (updateError) {
+        console.error("Error updating file path:", updateError);
+        Swal.fire({
+          // Display error alert
+          icon: "error",
+          title: "Oops...",
+          text: "Failed to update the file path in the database!",
+        });
+        return { error: updateError };
+      }
+
+      console.log("File path updated successfully:", updateData);
+      Swal.fire({
+        // Display success alert
+        icon: "success",
+        title: "Uploaded",
+        text: "Your file has been uploaded and the file path is updated.",
+      });
+      return { data: updateData };
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      Swal.fire({
+        // Display error alert
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+      return { error };
+    }
+  };
+
+  const handleFileDownload = async () => {
+    try {
+      const { data, error } = await supabase.storage
+        .from("quotation")
+        .download(quotationPath);
+      const blob = new Blob([data], { type: "application/octet-stream" });
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      const fileName = quotationPath.split("/").pop();
+      link.download = fileName;
+      link.click();
+      URL.revokeObjectURL(url);
+
+      if (error) {
+        window.alert("Error downloading file!");
+        return;
+      }
+    } catch (error) {
+      window.alert("Error downloading file!");
+    }
+  };
+
   const renderContent = () => {
     // Quotation Feedback
     if (quotationRequired === "true" || quotationRequired === null) {
       return (
         <div>
-          <UploadQuotation
-            bucketName="quotation"
-            ServiceRequestID={serviceTicket.ServiceRequestID}
-          />
+          <input type="file" onChange={handleFileChange} />
         </div>
       );
     } else if (quotationRequired === "false") {
@@ -224,7 +340,10 @@ const ViewTicketStaff = () => {
         _______________________________________
         <SubmittedByCard tenant={tenant} />
         _______________________________________
-        <DisplayQuotation ServiceRequestID={serviceTicket.ServiceRequestID} />
+        <div>
+          <button onClick={handleFileDownload}>Download File</button>
+        </div>
+        <DisplayQuotation quotationPath={quotationPath} file={file} />
         _______________________________________
       </div>
     );
@@ -241,7 +360,10 @@ const ViewTicketStaff = () => {
           <button onClick={handleStartWorks}>Start Works</button>
         </div>
         _______________________________________
-        <DisplayQuotation ServiceRequestID={serviceTicket.ServiceRequestID} />
+        <div>
+          <button onClick={handleFileDownload}>Download File</button>
+        </div>
+        <DisplayQuotation quotationPath={quotationPath} file={file} />
         _______________________________________
       </div>
     );
@@ -266,7 +388,10 @@ const ViewTicketStaff = () => {
           <h2>Rejection Details</h2>
           <p>Reason for rejection: {feedbackComments}</p>
         </div>
-        <DisplayQuotation ServiceRequestID={serviceTicket.ServiceRequestID} />
+        <div>
+          <button onClick={handleFileDownload}>Download File</button>
+        </div>
+        <DisplayQuotation quotationPath={quotationPath} file={file} />
         _______________________________________
       </div>
     );
@@ -283,7 +408,14 @@ const ViewTicketStaff = () => {
           <button onClick={handleEndWorks}>End Works</button>
         </div>
         ____________________________________
-        <DisplayQuotation ServiceRequestID={serviceTicket.ServiceRequestID} />
+        {quotationRequired && (
+          <div>
+            <div>
+              <button onClick={handleFileDownload}>Download Quotation</button>
+            </div>
+            <DisplayQuotation quotationPath={quotationPath} file={file} />
+          </div>
+        )}
       </div>
     );
   }
@@ -294,6 +426,15 @@ const ViewTicketStaff = () => {
         <BasicTicketDetails ticket={serviceTicket} />
         _______________________________________
         <SubmittedByCard tenant={tenant} />
+        _______________________________________
+        {quotationRequired && (
+          <div>
+            <div>
+              <button onClick={handleFileDownload}>Download Quotation</button>
+            </div>
+            <DisplayQuotation quotationPath={quotationPath} file={file} />
+          </div>
+        )}
       </div>
     );
   }
@@ -311,6 +452,15 @@ const ViewTicketStaff = () => {
 
           <button onClick={handleStartWorks}>Restart Works</button>
         </div>
+        _______________________________________
+        {quotationRequired && (
+          <div>
+            <div>
+              <button onClick={handleFileDownload}>Download Quotation</button>
+            </div>
+            <DisplayQuotation quotationPath={quotationPath} file={file} />
+          </div>
+        )}
       </div>
     );
   }
@@ -326,6 +476,15 @@ const ViewTicketStaff = () => {
           rating={serviceTicket.FeedbackRating}
           comments={serviceTicket.FeedbackComments}
         />
+        _______________________________________
+        {quotationRequired && (
+          <div>
+            <div>
+              <button onClick={handleFileDownload}>Download Quotation</button>
+            </div>
+            <DisplayQuotation quotationPath={quotationPath} file={file} />
+          </div>
+        )}
       </div>
     );
   }
