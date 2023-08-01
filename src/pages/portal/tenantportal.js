85b33671950dcdcef5f49ad56c9fab5abbcf5c58
CwjNavi <ivan_chan@mymail.sutd.edu.sg>
2023-08-01 19:06:27 +0800

    refactor: removed excess imports from tenant portal

--- src/pages/portal/tenantportal.js
+++ src/pages/portal/tenantportal.js
@@ -6,32 +6,17 @@ import "../../styles/ticketportal.css";
 import Cookies from "js-cookie";
 
 // material ui
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import Button from '@mui/material/Button';
-import { visuallyHidden } from '@mui/utils';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
 import SortIcon from '@mui/icons-material/Sort';
 import SearchIcon from '@mui/icons-material/Search';
-import { Height } from "@mui/icons-material";
 
 const TenantPortal = () => {
   const ticketManager = new TicketManager();
