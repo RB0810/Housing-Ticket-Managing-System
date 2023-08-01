9a03a7f3e41a66ab54a3ba7a09cd1d84e4977acc
CwjNavi <ivan_chan@mymail.sutd.edu.sg>
2023-08-01 19:05:30 +0800

    styles: supervisor landing portal

--- src/pages/portal/supervisorportal.js
+++ src/pages/portal/supervisorportal.js
@@ -4,6 +4,19 @@ import TicketManager from "../../managers/ticketmanager";
 import "../../styles/ticketportal.css";
 import Cookies from "js-cookie";
 
+// material ui
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import SortIcon from '@mui/icons-material/Sort';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 export default function SupervisorPortal() {
   const ticketManager = new TicketManager();
@@ -72,6 +85,22 @@ export default function SupervisorPortal() {
       setIsLoading(false);
     }
   };
+
+// implement get units from tenant id
+  // useEffect(() => {
+  //   // Fetch unit data from the server and populate the dropdown options
+  //   const fetchUnits = async () => {
+  //     try {
+  //       const unitData = await accountManager.getUnits(TenantID);
+  //       setUnits(unitData);
+  //     } catch (error) {
+  //       console.error("Error fetching units:", error);
+  //     }
+  //   };
+
+  //   fetchUnits();
+  // }, [TenantID]);
+
 
   const getViewTicketsRoute = () => {
     return "/supervisorportal/ticket";
@@ -169,7 +198,184 @@ export default function SupervisorPortal() {
 
   return (
     <div className="page supervisorportal">
-      <table className="ticket-portal-table">
+
+      {/* ############### MUI table start ################ */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ height: 100}}>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">S/N</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Ticket ID<IconButton
+                onClick={() => setIsRequestFilterOpen(!isRequestFilterOpen)}><SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isRequestFilterOpen ? ' open' : ''}`}>
+                    <input
+                      type="text"
+                      value={requestFilter}
+                      onChange={(e) => setRequestFilter(e.target.value)}
+                      placeholder="Filter by request..."
+                    />
+                  </div>
+                )}
+              </TableCell>
+
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Category<IconButton onClick={() => setIsCategoryFilterOpen(!isCategoryFilterOpen)}>
+                <SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isCategoryFilterOpen ? ' open' : ''}`}>
+                    <select
+                      value={categoryFilter}
+                      onChange={(e) => setCategoryFilter(e.target.value)}>
+                      <option value="">All</option>
+                      <option value="Plumbing">Plumbing</option>
+                      <option value="Toilet">Toilet</option>
+                      <option value="Pest">Pest</option>
+                      <option value="Electric">Electric</option>
+                      <option value="Aircon">Aircon</option>
+                      <option value="Others">Others</option>
+                    </select>
+                  </div>
+                )}</TableCell>
+
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Unit<IconButton onClick={() => setIsUnitFilterOpen(!isUnitFilterOpen)}>
+                <SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isUnitFilterOpen ? ' open' : ''}`}>
+                    <select
+                      value={unitFilter}
+                      onChange={(e) => setUnitFilter(e.target.value)}>
+                      <option value="">All</option>
+                      {/* {units.map((unit) => (
+                        <option key={unit.UnitNumber} value={unit.UnitNumber}>
+                          {unit.UnitNumber}
+                        </option>
+                      ))} */}
+                    </select>
+                  </div>
+                )}
+              </TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Status<IconButton onClick={() => setIsStatusFilterOpen(!isStatusFilterOpen)}>
+                <SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isStatusFilterOpen ? ' open' : ''}`}>
+                    <select
+                      value={statusFilter}
+                      onChange={(e) => setStatusFilter(e.target.value)}
+                    >
+                      <option value="">All</option>
+                      <option value="Awaiting Review">Awaiting Review</option>
+                      <option value="Ticket Assigned">Ticket Assigned</option>
+                      <option value="Quotation Uploaded">Quotation Uploaded</option>
+                      <option value="Quotation Accepted">Quotation Accepted</option>
+                      <option value="Quotation Rejected">Quotation Rejected</option>
+                      <option value="Works Started">Works Started</option>
+                      <option value="Works Ended">Works Ended</option>
+                      <option value="Works Rejected">Works Rejected</option>
+                      <option value="Feedback Submitted">Feedback Submitted</option>
+                    </select>
+                  </div>
+                )}</TableCell>
+
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Submitted By<IconButton
+                onClick={() => setIsRequestFilterOpen(!isSubmittedByFilterOpen)}><SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isSubmittedByFilterOpen ? ' open' : ''}`}>
+                    <input
+                      type="text"
+                      value={submittedByFilter}
+                      onChange={(e) => setSubmittedByFilter(e.target.value)}
+                      placeholder="Filter by submitted..."
+                    />
+                  </div>
+                )}
+              </TableCell>
+
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Submitted Date<IconButton
+                onClick={() => setIsDateFilterOpen(!isDateFilterOpen)}><SortIcon></SortIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isDateFilterOpen ? ' open' : ''}`}>
+                    <select
+                      value={dateFilter}
+                      onChange={(e) => setDateFilter(e.target.value)}
+                    >
+                      <option value="">Sort according to Date Submitted</option>
+                      <option value="newest">Newest to Oldest</option>
+                      <option value="oldest">Oldest to Newest</option>
+                    </select>
+                  </div>
+                )}
+              </TableCell>
+
+              <TableCell sx={{ fontWeight: 'bold' }} align="left">Assigned To<IconButton onClick={() => setIsAssignedToFilterOpen(!isAssignedToFilterOpen)}>
+                <SearchIcon></SearchIcon></IconButton>
+                {(
+                  <div className={`filter-dropdown${isAssignedToFilterOpen ? ' open' : ''}`}>
+                    <input
+                      type="text"
+                      value={assignedToFilter}
+                      onChange={(e) => setAssignedToFilter(e.target.value)}
+                      placeholder="Filter by assigned..."
+                    />
+                  </div>
+                )}</TableCell>
+
+              <TableCell align="left"><Button
+              sx={{backgroundColor: '#e91e63', '&:hover': {
+                backgroundColor: '#a31545',
+              }, }}
+              variant="contained" onClick={removeFilters}>Remove Filters</Button></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {isLoading ? (
+              <tr>
+                <td colSpan="8" className="text-center">
+                  Loading...
+                </td>
+              </tr>
+            ) : fetchError ? (
+              <tr>
+                <td colSpan="8" className="text-center">
+                  {fetchError}
+                </td>
+              </tr>
+            ) : filteredTickets.length === 0 ? (
+              <tr>
+                <td colSpan="8" className="text-center">
+                  Empty!
+                </td>
+              </tr>
+            ) :
+
+              (filteredTickets.map((ticket, index) => (
+                <TableRow
+                  hover
+                  key={ticket.ServiceRequestID}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="left">{index + 1}</TableCell>
+                  <TableCell align="left">{ticket.ServiceRequestID}</TableCell>
+                  <TableCell align="left">{ticket.Name}</TableCell>
+                  <TableCell align="left">{ticket.Category}</TableCell>
+                  <TableCell align="left">{ticket.Property}</TableCell>
+                  <TableCell align="left">{ticket.Status}</TableCell>
+                  <TableCell align="left">{new Date(ticket.SubmittedDateTime).toLocaleDateString()}</TableCell>
+                  <TableCell className="text-center">{ticket.tenantDetails ? ticket.tenantDetails.TenantName : null}</TableCell>
+                  <TableCell align="left">{ticket.staffDetails ? ticket.staffDetails.StaffName : "Unassigned"}</TableCell>
+                  <TableCell align="left">
+                    <Link to={`${getViewTicketsRoute()}/${SupervisorID}/${ticket.ServiceRequestID}`}>
+                      <Button variant="contained" >View Ticket
+                      </Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              )))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* MUI table end */}
+
+      {/* <table className="ticket-portal-table">
         <thead>
           <tr className="table-row">
             <th className="header-cell text-center">Ticket ID</th>
@@ -364,7 +570,7 @@ export default function SupervisorPortal() {
       </table>
       <button onClick={removeFilters}>
         Remove all Filters
-      </button>
+      </button> */}
     </div>
   );
 }
