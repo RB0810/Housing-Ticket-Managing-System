d495071beb6694c90dac85888638c010d8636c04
Lucas Toh <tohhylucas@gmail.com>
2023-08-03 13:09:36 +0800

    Update tenant portal testing

--- features/step_definitions/tenantportal.feature
+++ features/step_definitions/tenantportal.feature
@@ -5,42 +5,31 @@ Scenario: Login to Tenant Account
   When Tenant enters valid account credentials
   And Tenant clicks "login" button
   Then Tenant should be redirected to the Tenant home page
-  But if login is unsuccessful, an error notification for invalid credentials should appear
-
-Scenario: View all pending tickets
-  Given Tenant has logged into their tenant account successfully
-  When Tenant navigates to the landing page
-  And Tenant clicks on "View Pending Tickets"
-  Then Tenant should see all pending tickets in a table format
-
-Scenario: View all active tickets
-  Given Tenant has logged into their tenant account successfully
-  When Tenant navigates to the landing page
-  And Tenant clicks on "View Active Tickets"
-  Then Tenant should see all active tickets in a table format
-
-Scenario: View all closed tickets
-  Given Tenant has logged into their tenant account successfully
-  When Tenant navigates to the landing page
-  And Tenant clicks on "View Closed Tickets"
-  Then Tenant should see all closed tickets in a table format
 
 Scenario: Create new service ticket
-  Given Tenant is logged in
-  When Tenant navigates to the landing page
-  And Tenant clicks on "Create Ticket"
+  Given Tenant clicks on "Create Ticket"
   And Tenant fills in the required information
   And Tenant clicks "Create Service Ticket"
   Then Tenant should receive a success notification
 
+Scenario: View all pending tickets
+  Given Tenant clicks on "View Pending Tickets"
+  Then Tenant should see all pending tickets in a table format
+
+Scenario: View all active tickets
+  Given Tenant clicks on "View Active Tickets"
+  Then Tenant should see all active tickets in a table format
+
+Scenario: View all closed tickets
+  Given Tenant clicks on "View Closed Tickets"
+  Then Tenant should see all closed tickets in a table format
+
 Scenario: View Tenant Account Information
-  Given Tenant is logged in
-  When Tenant navigates to the "Profile" section through the Navbar
+  Given Tenant navigates to the "Profile" section through the Navbar
   Then Tenant's account information should be displayed
 
 Scenario: Set new password
-  Given Tenant is logged in and in the profile page
-  When Tenant goes to the Profile page via NavBar
+  Given Tenant is in the "View Tenant Account Information" page
   And Tenant enters a new password
   And Tenant re-enters the new password
   And Tenant clicks on "Set New Password"
@@ -48,7 +37,6 @@ Scenario: Set new password
 
 Scenario: View Quotation
   Given Ticket status is Quotation uploaded
-  When Tenant receives a notification of Quotation uploaded
   And Tenant clicks on "View Ticket"
   Then Tenant should see the Quotation rendered using ReactPDFViewer
 
--- features/step_definitions/tenantportal.js
+++ features/step_definitions/tenantportal.js
@@ -1,9 +1,122 @@
-const { Given, When, Then } = require("@cucumber/cucumber");
+const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
 const { Builder, By, Key, until, Select } = require("selenium-webdriver");
 const assert = require("assert");
 const chrome = require("selenium-webdriver/chrome");
+const { createClient } = require("@supabase/supabase-js");
+
+// Setup Supabase for Testing
+const supabaseUrl = "https://mnfsjgaziftztwiarlys.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZnNqZ2F6aWZ0enR3aWFybHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4MjkwODgsImV4cCI6MjAwMjQwNTA4OH0.Mrvmdish7OlO5-m1WIZTNwVFUnEcF7aoHE53ZVwiOY8";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 let driver;
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+Before(async function () {
+  let { data, error } = await supabase // Create a new ticket
+    .from("Service Request")
+    .insert([
+      {
+        ServiceRequestID: 999999,
+        Name: "TESTINGTICKETJEST",
+        TenantID: 999,
+        PARCStatus: "PENDING",
+        SubmittedDateTime: "2021-04-01 00:00:00",
+        Category: "TESTINGCATEGORYJEST",
+      },
+    ])
+    .select();
+  if (error) {
+    throw error;
+  } else {
+    console.log("Created a new PENDING ticket for testing.");
+  }
+
+  let { data2, error2 } = await supabase // Create a new ticket
+    .from("Service Request")
+    .insert([
+      {
+        ServiceRequestID: 999998,
+        Name: "TESTINGTICKETJEST",
+        TenantID: 999,
+        PARCStatus: "ACTIVE",
+        SubmittedDateTime: "2021-04-01 00:00:00",
+        Category: "TESTINGCATEGORYJEST",
+      },
+    ])
+    .select();
+  if (error2) {
+    throw error2;
+  } else {
+    console.log("Created a new ACTIVE ticket for testing.");
+  }
+
+  let { data3, error3 } = await supabase // Create a new ticket
+    .from("Service Request")
+    .insert([
+      {
+        ServiceRequestID: 999997,
+        Name: "TESTINGTICKETJEST",
+        TenantID: 999,
+        PARCStatus: "CLOSED",
+        SubmittedDateTime: "2021-04-01 00:00:00",
+        Category: "TESTINGCATEGORYJEST",
+      },
+    ])
+    .select();
+  if (error3) {
+    throw error3;
+  } else {
+    console.log("Created a new CLOSED ticket for testing.");
+  }
+});
+
+After(async function () {
+  let { data, error } = await supabase // Delete Created Tickets
+    .from("Service Request")
+    .delete()
+    .match({ ServiceRequestID: 999999 });
+  if (error) {
+    throw error;
+  } else {
+    console.log("Deleted PENDING ticket for testing.");
+  }
+
+  let { data2, error2 } = await supabase // Delete Created Tickets
+    .from("Service Request")
+    .delete()
+    .match({ ServiceRequestID: 999998 });
+  if (error2) {
+    throw error2;
+  } else {
+    console.log("Deleted ACTIVE ticket for testing.");
+  }
+
+  let { data3, error3 } = await supabase // Delete Created Tickets
+    .from("Service Request")
+    .delete()
+    .match({ ServiceRequestID: 999997 });
+  if (error3) {
+    throw error3;
+  } else {
+    console.log("Deleted CLOSED ticket for testing.");
+  }
+
+  let { data4, error4 } = await supabase // Delete Created Tickets
+    .from("Service Request")
+    .delete()
+    .match({ Name: "DELETEME" });
+  if (error4) {
+    throw error;
+  } else {
+    console.log("Deleted created ticket for testing.");
+  }
+});
 
 Given("Tenant has loaded the login page for Tenant Portal", async function () {
   driver = await new Builder()
@@ -11,33 +124,95 @@ Given("Tenant has loaded the login page for Tenant Portal", async function () {
     .setChromeOptions(new chrome.Options())
     .build();
   await driver.get("http://localhost:3000/tenantlogin");
+  await driver.manage().setTimeouts({ implicit: 1 }); // Set a timeout for implicit waits (e.g., 10 seconds)
+  await driver.wait(until.elementLocated(By.className("wlcText")));
   assert.equal(
     await driver.findElement(By.className("wlcText")).getText(),
     "Tenant Portal\nLogin"
   );
+
+  await delay(1000); // Wait for 1 second (1000 milliseconds
 });
 
 When("Tenant enters valid account credentials", async function () {
-  await driver.findElement(By.name("ID")).sendKeys("testtenant@gmail.com");
-  await driver.findElement(By.name("password")).sendKeys("testtenant123");
-  await driver.findElement(By.className("loginBtn")).click();
+  const usernameInput = await driver.findElement(
+    By.css('input[type="text"].MuiInputBase-input')
+  );
+  const passwordInput = await driver.findElement(
+    By.css('input[type="password"].MuiInputBase-input')
+  );
+  await usernameInput.sendKeys("testtenant@gmail.com");
+  await passwordInput.sendKeys("testtenant123");
+
+  await delay(1000); // Wait for 1 second (1000 milliseconds
+});
+
+When('Tenant clicks "login" button', async function () {
+  await driver.findElement(By.className("login-portal-button")).click();
+
+  await delay(1000); // Wait for 1 second (1000 milliseconds
 });
 
 Then("Tenant should be redirected to the Tenant home page", async function () {
-  assert.equal(
-    await driver.getCurrentUrl(),
-    "http://localhost:3000/tenantportal/landingpage/999"
+  await assert.equal(
+    assert.equal(
+      await driver.findElement(By.className("site-title")).getText(),
+      "Tenant Portal"
+    )
   );
+
+  await delay(1000);
 });
 
-Given("Tenant has loaded the Tenant home page", async function () {
-  assert.equal(
-    await driver.findElement(By.className("site-title")).getText(),
-    "Tenant Portal"
+Given('Tenant clicks on "Create Ticket"', async function () {
+  const linkElement = await driver.findElement(
+    By.xpath(`//a[@href='/tenantportal/createticket/999']`)
   );
+  await linkElement.click();
+
+  await delay(1000);
 });
 
-And("Tenant clicks on 'View Pending Tickets'", async function () {
-    await driver.findElement(By.className("viewPendingTicketsBtn")).click();
-    
+When("Tenant fills in the required information", async function () {
+  await driver
+    .findElement(By.id("tenant-create-ticket-name-textfield"))
+    .sendKeys("DELETEME");
+
+  await driver.findElement(By.id("tenant-create-ticket-request-type-select"));
+
+  await driver
+    .findElement(By.id("tenant-create-ticket-category-textfield"))
+    .click();
 });
+
+When("Tenant clicks on 'Submit'", async function () {});
+
+When("Tenant clicks on 'View Pending Tickets'", async function () {
+  await driver
+    .findElement(By.xpath("//img[contains(@src, 'pendingticket.png')]"))
+    .click();
+});
+
+Then(
+  "Tenant should see all pending tickets in a table format",
+  async function () {}
+);
+
+Given(
+  "Tenant has logged into their tenant account successfully",
+  async function () {
+    let site_title = await driver.findElement(By.className("site-title"));
+    assert.equal(await site_title.getText(), "Tenant Portal");
+  }
+);
+
+When("Tenant navigates to the landing page'", async function () {
+  await driver.findElement(By.className("viewPendingTicketsBtn")).click();
+});
+
+When("Tenant clicks on 'View Pending Tickets'", async function () {});
+
+Then(
+  "Tenant should see all pending tickets in a table format",
+  async function () {}
+);
