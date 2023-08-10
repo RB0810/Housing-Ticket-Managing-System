e99f29b7e4ea7db449fbb58d448084f03e03adb2
Rishika Banka <91309421+RB0810@users.noreply.github.com>
2023-08-10 09:47:07 +0800

    fix .env

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-console.log(process.env.REACT_APP_SUPABASE_KEY);
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
